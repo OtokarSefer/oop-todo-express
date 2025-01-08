67e3cd9ebe72f7277da66d394bbf121701685f8d
OtokarSefer <Otokar.Sefer@voco.ee>
2025-01-08 15:35:20 +0200

    delete maybe

--- controllers/todos.js
+++ controllers/todos.js
@@ -1,44 +1,81 @@
-import {Todo} from '../models/todo.js'
+import { Todo } from '../models/todo.js';
 
-class todoController {
-    constructor() {
-        this.TODOS = []
+class TodoController {
+  constructor() {
+    this.TODOS = [];
+  }
+
+  /**
+   * Creates a new Todo item.
+   * @param {Object} req - The request object.
+   * @param {Object} res - The response object.
+   */
+  createTodo(req, res) {
+    const task = req.body.task;
+    const newTodo = new Todo(Math.random().toString(), task);
+    this.TODOS.push(newTodo);
+    res.json({
+      newTask: newTodo,
+    });
+  }
+
+  /**
+   * Retrieves all Todo items.
+   * @param {Object} req - The request object.
+   * @param {Object} res - The response object.
+   */
+  getTodos(req, res) {
+    res.json({ tasks: this.TODOS });
+  }
+
+  /**
+   * Updates a Todo item.
+   * @param {Object} req - The request object.
+   * @param {Object} res - The response object.
+   */
+  updateTodo(req, res) {
+    const todoId = req.params.id;
+    const updateTask = req.body.task;
+    console.log(req.body);
+    console.log(req.params);
+
+    const todoIndex = this.TODOS.findIndex((todo) => todo.id === todoId);
+
+    if (todoIndex < 0) {
+      res.status(404).json({
+        message: 'Could not find todo with such index',
+      });
+      return;
     }
 
-    createTodo(req, res) {
-        const task = req.body.task
-        const newTodo = new Todo(Math.random().toString(), task)
-        this.TODOS.push(newTodo)
-        res.json({
-            newTask: newTodo
-        })
+    this.TODOS[todoIndex] = new Todo(this.TODOS[todoIndex].id, updateTask);
+    res.json({
+      message: 'Todo is updated',
+      updateTask: this.TODOS[todoIndex],
+    });
+  }
+
+  /**
+   * Deletes a Todo item.
+   * @param {Object} req - The request object.
+   * @param {Object} res - The response object.
+   */
+  deleteTodo(req, res) {
+    const todoId = req.params.id;
+    const todoIndex = this.TODOS.findIndex((todo) => todo.id === todoId);
+
+    if (todoIndex < 0) {
+      res.status(404).json({
+        message: 'Could not find todo with such index',
+      });
+      return;
     }
 
-    getTodos(req, res){
-        res.json({tasks: this.TODOS})
-    }
-
-    updateTodo(req, res){
-        const todoId = req.params.id
-        const updateTask = req.body.task
-        console.log(req.body)
-        console.log(req.params)
-
-        const todoIndex = this.TODOS.findIndex((todo) => todo.id == todoId)
-
-        if(todoIndex < 0){
-            res.json({
-                message: 'Cloud not find todo with such index'
-            })
-            throw new Error('Could not find todo')
-        }
-
-        this.TODOS[todoIndex] = new Todo(this.TODOS[todoIndex].id, updateTask)
-        res.json({
-            message: 'todo is updated',
-            updateTask: this.TODOS[todoIndex]
-        })
-    }
+    this.TODOS.splice(todoIndex, 1);
+    res.json({
+      message: 'Todo is deleted',
+    });
+  }
 }
 
-export const TodoController = new todoController()
+export const TodoController = new TodoController();
